dc25d1d41b22ea1c829553de5fab14b5ee7172ac
Nick Sonner <nicksonner@gmail.com>
2017-11-29 11:49:48 +1100

    add onChangeFirstName function

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ import './App.css';
 class App extends Component {
   state = {
     user: {
-      firstName: 'Aubrey',
-      lastName: 'Porter',
-      profileImage: 'https://randomuser.me/api/portraits/women/48.jpg'
+      firstName: 'Citizen',
+      lastName: 'Person',
+      // profileImage: 'https://randomuser.me/api/portraits/women/48.jpg'
+      profileImage: 'https://pbs.twimg.com/profile_images/887872349743087616/BC_InNdz_400x400.jpg'
     }
+  }
+  
+  // when user types in text field
+  // this function is called
+  // the event provides info on what happened
+  onChangeFirstName = (event) => {
+     // get the <input>
+     const input = event.target
+     // get the current inputted text
+     const newFirstName = input.value
+    this.setState((prevState) => {
+      const user = prevState.user
+      user.firstName = newFirstName
+      return {
+        // this.state.user will be updated
+        user: user
+      }
+    })
   }
 
   render() {
@@ -19,6 +38,14 @@ class App extends Component {
         <h1>Profile Editor</h1>
         <img src={user.profileImage} />
         <p><strong>Name: </strong>{user.firstName} {user.lastName}</p>
+
+        <label>
+          First name: {' '}
+          <input 
+          value={user.firstName} 
+          onChange={this.onChangeFirstName}
+          />
+        </label>  
       </div>
     );
   }
