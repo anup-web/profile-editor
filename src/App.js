a72fd7b83659f181e6efde7190bc768225fa6607
Nick Sonner <nicksonner@gmail.com>
2017-11-29 11:12:30 +1100

    update readme

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    user: {
+      firstName: 'Aubrey',
+      lastName: 'Porter',
+      profileImage: 'https://randomuser.me/api/portraits/women/48.jpg'
+    }
+  }
+
   render() {
+    const user = this.state.user;
+
     return (
       <div className="App">
         <h1>Profile Editor</h1>
-        <img src='https://randomuser.me/api/portraits/women/48.jpg' />
-        <p><strong>Name:</strong>aubrey porter</p>
+        <img src={user.profileImage} />
+        <p><strong>Name: </strong>{user.firstName} {user.lastName}</p>
       </div>
     );
   }
